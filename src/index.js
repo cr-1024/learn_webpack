f606baa25764bb030f1ae44d7d553ad3541bbed0
v_rosiechen <v_rosiechen@tencent.com>
2019-08-28 19:38:54 +0800

    webpack 起步

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import _ from 'lodash';
+// import './style.css';
+// import Img from './images.jpg';
+// import Data from './data.xml';
+
+import printMe from './print.js';
+
 
 function component(){
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello','webpack'],' ');
+    // element.classList.add('hello');
+
+    // var myImg = new Image();
+    // myImg.src = Img;
+
+    // element.appendChild(myImg);
+
+    // console.log(Data);
+
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
